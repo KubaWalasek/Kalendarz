8de553cecaf698eb3a765ff22a63a7bf344c27f5
KubaWalasek <kubariddick@gmail.com>
2025-11-14 10:25:38 +0100

    zmiany w settings do railway

--- CalendarApp/settings.py
+++ CalendarApp/settings.py
@@ -36,6 +36,11 @@ CSRF_TRUSTED_ORIGINS = [
     "https://*.up.railway.app",
 ]
 
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = "Lax"
+
 
 # Application definition
 
