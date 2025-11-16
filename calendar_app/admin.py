88b816dfa5c4434e1ffb8b1fa9cd15ef86f689d4
KubaWalasek <kubariddick@gmail.com>
2025-11-16 10:32:05 +0100

    Zmiany pod cloudinary

--- calendar_app/admin.py
+++ calendar_app/admin.py
@@ -1,6 +1,6 @@
-from django.contrib import admin
-from .models import Reminder, DayNote
 
+from django.contrib import admin
+from .models import Reminder, DayNote, CalendarBackground
 
 @admin.register(Reminder)
 class ReminderAdmin(admin.ModelAdmin):
@@ -20,3 +20,21 @@ class DayNoteAdmin(admin.ModelAdmin):
     def short_text(self, obj):
         return (obj.text[:60] + "…") if len(obj.text) > 60 else obj.text
     short_text.short_description = "Tekst"
+
+
+@admin.register(CalendarBackground)
+class CalendarBackgroundAdmin(admin.ModelAdmin):
+    list_display = ['name', 'is_active', 'created_at', 'image']
+    list_filter = ['is_active', 'created_at']
+    actions = ['activate_background']
+
+    def activate_background(self, request, queryset):
+        # Wyłącz wszystkie
+        CalendarBackground.objects.update(is_active=False)
+        # Włącz wybrane (tylko pierwsze jeśli zaznaczono wiele)
+        first = queryset.first()
+        if first:
+            first.is_active = True
+            first.save()
+        self.message_user(request, f"Tło '{first.name}' zostało aktywowane")
+    activate_background.short_description = "Aktywuj wybrane tło"
