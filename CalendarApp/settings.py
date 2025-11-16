3a9fdefab9bdbcc8dcb1e6e7e942b02a1b657b59
KubaWalasek <kubariddick@gmail.com>
2025-11-16 09:56:44 +0100

    dodanie cloudianry

--- CalendarApp/settings.py
+++ CalendarApp/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
+    'cloudinary',
     'calendar_app',
     'accounts',
     'workout'
@@ -155,6 +157,14 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+}
+
+DEFAULT_FILE_STORAGE='cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # WhiteNoise tylko w produkcji
 if not DEBUG:
