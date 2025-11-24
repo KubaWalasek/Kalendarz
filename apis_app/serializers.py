f518d533dd6fad595badd60576e369b7bd63cfbf
KubaWalasek <kubariddick@gmail.com>
2025-11-24 14:34:40 +0100

    poprawka requirements - dodanie djangorestframework

--- apis_app/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-
-class WeatherSerializer(serializers.Serializer):
-    city = serializers.CharField()
-    temp = serializers.FloatField()
-    description = serializers.CharField()
-    icon = serializers.CharField()
-    error = serializers.CharField()
